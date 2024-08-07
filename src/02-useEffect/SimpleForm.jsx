3e94fb235a11a30d1de14c50119a696647ab3e82
Heimer <heimer79@gmail.com>
2024-08-07 16:12:49 -0500

    feat: Add SimpleForm component for user input

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+
+
+
+export const SimpleForm = () => {
+
+    const [ formState, setFormState ] = useState(
+        {
+            username: 'Heimer',
+            email: 'heimer79@gmail.com'
+        }
+     )
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+            
+        })
+
+        /* console.log({ name, value } ); */
+    }   
+
+
+    useEffect(
+        () => {
+            console.log('Hey!')
+        }, 
+    )
+
+
+
+
+
+
+    return (
+        <>
+
+            <h1>Simple Form</h1>
+            <hr />
+
+            <input 
+            type="text"
+            className="form-control mt-2"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={ onInputChange }
+
+            />
+
+            <input 
+            type="email"
+            className="form-contro mt-2"
+            placeholder="Email"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+            
+            />
+        
+        </>
+    )
+}
